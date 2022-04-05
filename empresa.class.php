b68f44a71ce29bb57ad3166ff584d5d0279f16ef
Renatinhoo <naruto24fatal@gmail.com>
2022-04-05 12:23:51 -0300

    Segunda versão

---
+++ empresa.class.php
@@ -0,0 +1,15 @@
+<?php
+
+Class empresa{
+    private $razao_social;
+
+    public function setRazaoSocial($razao_social){
+      $this->razao_social = $razao_social;  
+    }
+
+    public function getRazaoSocial(){
+        return $this->razao_social;
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 
 require_once("pessoa.class.php");
+require_once("empresa.class.php");
 
 $pessoa = new Pessoa();
 $pessoa->setNome("Renatinho Renato Botura da costa");
+$pessoa->setEndereco("<br> Rua dos mitos banana 2 ");
 echo $pessoa->getNome();
+echo $pessoa->getEndereco();
+
+$empresa = new Empresa();
+$empresa->setRazaoSocial("<br> Escola XPTO LTDS");
+echo $empresa->getRazaoSocial();
 
 ?>
